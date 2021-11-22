4c4971d0702614b3634e9453d3e680676bc366bf
Charles Doty <cdoty@users.noreply.github.com>
2021-11-22 05:51:04 -0600

    Moved code to shared folder
    Moved shared 6502 code to shared folder.

--- AppleII/Program.s
+++ AppleII/Program.s
@@ -4,9 +4,9 @@
 	!src "Defines.inc"
 	!src "RamDefines.inc"
 	!src "Startup.s"
-	!src "Title.s"
-	!src "Game.s"
 	!src "Routines.s"
-	!src "huffmunch.s"
-	!src "Data.s"
+	!src "../Shared/Title6502.s"
+	!src "../Shared/Game6502.s"
+	!src "../Shared/huffmunch.s"
+	!src "../Shared/Data6502.s"
 	
