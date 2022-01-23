f8938d3351473c5fd41ddef2849dbe3f2866652e
Charles Doty <cdoty@users.noreply.github.com>
2022-01-22 22:50:47 -0600

    Moved romwak to main Tools folder.

--- Pottens/mk.bat
+++ Pottens/mk.bat
@@ -4,7 +4,7 @@ IF ERRORLEVEL 1 goto errorOut
 if not exist "e:\Mame\Roms\pokerout" mkdir e:\Mame\Roms\pokerout
 IF ERRORLEVEL 1 goto errorOut
 
-Tools\romwak_x86.exe /h Burger.bin brk01_tms128a.6f f_tms128a.8f
+..\Tools\romwak_x86.exe /h Burger.bin brk01_tms128a.6f f_tms128a.8f
 IF ERRORLEVEL 1 goto errorOut
 
 move /Y *.?f e:\Mame\Roms\pokerout
