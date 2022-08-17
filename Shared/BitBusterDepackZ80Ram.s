abf5fb95e43e775fb452e4e89dbc8a67b7425bbd
Charles Doty <cdoty@users.noreply.github.com>
2022-08-16 22:53:03 -0500

    Added Ram decoding of Bit Buster
    Added a ram decoding version of Bit Buster.

---
+++ Shared/BitBusterDepackZ80Ram.s
@@ -0,0 +1,156 @@
+;-----------------------------------------------------------
+; BitBuster v1.2 RAM Depacker v1.1 - 16Kb version
+; HL = RAM/ROM source ; DE = VRAM destination
+;-----------------------------------------------------------
+decompressToVRAM:
+	ifdef	DISABLE_INTERRUPTS
+	di
+	endif
+
+	; Skips 4 bytes data header
+	inc		hl
+	inc		hl
+	inc		hl
+	inc		hl
+
+	; Initialization
+	ld		a, 128
+	exx
+
+	ld		de, 1
+	exx
+
+; Main depack loop
+Depack_loop:
+	add		a, a
+	jp		nz, nxt0
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt0:	
+	jp		c, Compressed
+
+	ldi
+	
+	jp		Depack_loop
+
+; Compressed data
+Compressed:
+	ld		c, (hl)
+	inc		hl
+
+Match:
+	ld		b, 0
+	bit		7, c
+	jr		z, Match1
+
+	add		a, a
+	jp		nz, nxt1
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt1:
+	rl		b
+	add		a, a
+	jp		nz, nxt2
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt2:	
+	rl		b
+	add		a, a
+	jp		nz, nxt3
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt3:	
+	rl		b
+	add		a, a
+	jp		nz, nxt4
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt4:
+	jp		c, Match1
+	
+	res		7, c
+
+Match1:
+	inc		bc
+	exx
+
+	ld		h, d
+	ld		l, e
+	ld		b, e
+
+Gamma_size:
+	exx
+	add		a, a
+	jp		nz, nxt5
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt5:	
+	exx
+	jp		nc, Gamma_size_end
+
+	inc		b
+	jp		Gamma_size
+
+Gamma_bits:
+	exx
+	add		a, a
+	jp		nz, nxt6
+
+	ld		a, (hl)
+	inc		hl
+	rla
+
+nxt6:
+	exx
+	adc		hl, hl
+
+Gamma_size_end:
+	djnz	Gamma_bits
+
+	inc		hl
+	exx
+	jp		c, Depack_out
+
+	push	hl
+	exx
+
+	push	hl
+	exx
+
+	ld		h, d
+	ld		l, e
+	sbc		hl, bc
+
+	pop		bc
+
+	ldir
+
+	pop		hl
+	
+	jp		Depack_loop
+
+; Depacker exit
+Depack_out:
+	ifdef	DISABLE_INTERRUPTS
+	ei
+	endif
+
+	ret
