cde863f970851c96d48613e0c1b79b73039582eb
Charles Doty <cdoty@users.noreply.github.com>
2022-08-22 01:18:51 -0500

    Project cleanup.

--- Creativision/debugcas.bat
+++ Creativision/debugcas.bat
@@ -1,6 +1,6 @@
 set WORKING_DIRECTORY=%cd%
 
 cd \mame
-mame lasr2001 -cass %WORKING_DIRECTORY%\BurgerCreativision.wav -debug -autoboot_delay 6 -autoboot_command "BLOAD\nCALL -138"
+mame lasr2001 -cass %WORKING_DIRECTORY%\BurgerCreativision.wav -debug -autoboot_delay 2 -autoboot_command "BLOAD\nCALL -138"
 
 cd %WORKING_DIRECTORY%
